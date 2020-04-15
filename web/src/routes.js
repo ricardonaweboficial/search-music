5b74d686f89db8ed6b49064736648ac745d4fc96
RicardoWorf <steve4137@gmail.com>
2020-04-14 22:40:32 -0300

    Application for Search Musics

---
+++ web/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './global.css';
+
+import Home from './pages/Home';
+import Music from './pages/Music';
+
+export default function Routes() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/" exact component={Home}/>
+				<Route path="/music/:idTrack" component={Music}/>
+			</Switch>
+		</BrowserRouter>
+	);
+}
