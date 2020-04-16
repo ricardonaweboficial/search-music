270738204008086eea2ead9a08088bb793b380a4
RicardoWorf <steve4137@gmail.com>
2020-04-15 22:32:46 -0300

    Created Page for info and video of music

--- web/src/pages/Music/index.js
+++ web/src/pages/Music/index.js
@@ -1,12 +1,75 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, Fragment } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+
+import { FiArrowLeft } from 'react-icons/fi';
+
+import {
+	InfoContainer,
+	ButtonBack,
+	InfoVideo,
+	Video,
+	InfoMusic,
+	InfoGroup,
+	Description
+} from './styles';
 
 
 export default function Music() {
-	const { idTrack } = useParams();
+	const [ music, setMusic ] = useState([]);
+
+	const { idVideo } = useParams();
+
+	const history = useHistory();
+
+	useEffect(() => {
+		async function loadMusic() {
+			try {
+				const response = await fetch(`https://theaudiodb.com/api/v1/json/1/track.php?h=${idVideo}`);
+				const data = await response.json();
+				setMusic(data.track);
+				console.log(data.track);
+			} catch (err) {
+				return alert('Error' + err)
+			}
+		}
+
+		loadMusic();
+	}, []);
+
+	function handleBack() {
+		history.push('/');
+	}
 
 	return (
-		<h1>Id Music: {idTrack}</h1>
+		<InfoContainer>
+			<ButtonBack onClick={handleBack}><FiArrowLeft size={16} color="#fff" />Back</ButtonBack>
+			<section>
+				{music.map(info => (
+					<Fragment>
+						<InfoVideo>
+							<Video src={`http://www.youtube.com/embed/${info.strMusicVid.slice(-11)}`}
+							  allowFullScreen
+							></Video>
+							<InfoMusic>
+								<InfoGroup>
+									<h3>Name: <span>{info.strTrack}</span></h3>
+									<h3>Artist: <span>{info.strArtist}</span></h3>
+								</InfoGroup>
+
+								<InfoGroup>
+									<h4>Album: <span>{info.strAlbum}</span></h4>
+									<h4>Gênero: <span>{info.strGenre}</span></h4>
+								</InfoGroup>
+							</InfoMusic>
+						</InfoVideo>
+						<Description>
+							<h1>Description</h1>
+							<p>{info.strDescriptionEN}</p>
+						</Description>
+					</Fragment>
+				))}
+			</section>
+		</InfoContainer>
 	);
 
 }
---
+++ web/src/pages/Music/styles.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+export const InfoContainer = styled.div`
+	background:#f1f1f1;
+	width:100%;
+	max-width:1120px;
+	padding:40px;
+	height:100vh;
+
+	margin:0 auto;
+
+	section {
+		display:flex;
+		justify-content: space-around;
+		background:#424242;
+		padding:20px;
+		margin-top:30px;
+		color:#F6F6F6;
+	}
+`;
+
+export const ButtonBack = styled.button`
+	background:#424242;
+	border:0;
+	border-radius:4px;
+	color:#F6F6F6;
+	padding:10px 40px;
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	cursor:pointer;
+	svg {
+		margin-right:8px;
+	}
+`;
+
+export const InfoVideo = styled.div`
+	width:50%;
+	display:flex;
+	flex-direction:column;
+`;
+
+export const Video = styled.iframe`
+	width:100%;
+	height:100%;
+	border-radius:4px;
+	border:0;
+	margin-bottom:10px;
+`;
+
+
+export const InfoMusic = styled.div`
+	display:flex;
+	justify-content:space-between;	
+`;
+
+export const InfoGroup = styled.div`
+	display:flex;
+	flex-direction:column;
+	h3, h4 {
+		color:#ADADAD;
+	}
+
+	span {
+		color:#F6F6F6;
+	}
+`;
+
+export const Description = styled.div`
+	width:50%;
+	padding:0 30px;
+	display:flex;
+	justify-content:center;
+	flex-direction:column;
+	overflow:auto;
+	h1 {
+		text-align:center;
+		font-size:40px;
+		margin-bottom:10px;
+	}
+
+	p {
+		color: #f1f1f1;
+		text-align: justify;
+		text-justify:distribute;
+		height:300px;
+	}
+`;
+
